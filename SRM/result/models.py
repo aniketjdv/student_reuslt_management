892de7040b6c66bd20e3c4c6a3fccec1a3981caf
Aniket <aniketjadhav310@gmail.com>
2025-12-23 13:11:19 +0530

    do some work on demo

--- SRM/result/models.py
+++ SRM/result/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Studentdemo(models.Model):
+    STYPE = [('IT','Information Technology'),
+             ('CS','Computer Science'),
+             ('MBA','Buisness Admstration')
+             ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='img/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=3,choices=STYPE)
